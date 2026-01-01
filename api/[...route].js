a3564535b0796cbcd3e98a2600f7fbaa8c9e6102
Blog Setup <setup@blog.local>
2026-01-01 21:34:29 +0530

    Fix .vercelignore: keep backend for build, exclude only non-essential files

---
+++ api/[...route].js
@@ -0,0 +1,3 @@
+const app = require('../backend/dist/index.js');
+
+export default app;
--- api/index.js
+++ api/index.js
@@ -1,4 +1,1 @@
-// Simple wrapper that loads the compiled backend
-const app = require('../backend/dist/index.js');
-
-module.exports = app;
+// Empty - not used, routes go to [...route].js
